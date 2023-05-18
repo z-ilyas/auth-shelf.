379f74a8052e4ce1a48803cf5c228137881eef4d
Ilyas Mohamed <zswaleh@ymail.com>
2023-05-18 14:29:08 -0500

    made a button for shelf page, the input dataa is being sent via dispatch to shelf saga, we also made a yield axios post to the shelf route server'

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
 
 function ShelfPage() {
+
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState('');
 
   const dispatch = useDispatch();
 
@@ -10,10 +14,35 @@ function ShelfPage() {
     dispatch({ type: 'SAGA_FETCH_SHELF'});
 }, []);
 
+  const createShelf = () => {
+    dispatch ({ 
+      type: 'SAGA_CREATE_SHELF',
+      payload: {
+        description: description,
+        image: image
+      }
+    })
+  }
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      <input
+        placeholder='description'
+        type="text"
+        value={description}
+        required
+        onChange={(event) => setDescription(event.target.value)}    
+        />
+        <input
+        placeholder='image'
+        type="text"
+        value={image}
+        required
+        onChange={(event) => setImage(event.target.value)}    
+        />
+      <button onClick={createShelf}>Create Shelf</button>
     </div>
   );
 }
--- src/redux/sagas/shelf.sagas.js
+++ src/redux/sagas/shelf.sagas.js
@@ -10,8 +10,17 @@ function* fetchShelf() {
         console.log('Shelf get request failed', error);
     }
 }
+function* createShelf() {
+    try{
+        yield axios.post('/api/shelf', action.payload);
+        yield put({ type: 'SAGA_FETCH_SHELF' });
+    } catch (error) {
+        console.log('Shelf POST request failed', error);
+    }
+}
 function* ShelfSaga() {
     yield takeLatest('SAGA_FETCH_SHELF', fetchShelf);
+    yield takeLatest('SAGA_CREATE_SHELF', createShelf);
   }
   
 export default ShelfSaga;
