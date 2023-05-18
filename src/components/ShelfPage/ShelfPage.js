8bf5a71a5657f324fc3de64c5d7f34dfdfe0fff9
Ilyas Mohamed <zswaleh@ymail.com>
2023-05-18 13:37:42 -0500

    made shelf-saga and shelf-reducer and connected them to the necessary components, we also put a dispatch on shelf page

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
 
 function ShelfPage() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'SAGA_FETCH_SHELF'});
+}, []);
+
   return (
     <div className="container">
       <h2>Shelf</h2>
---
+++ src/redux/sagas/shelf.sagas.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* fetchShelf() {
+    try{
+        const response = yield axios.get('/api/shelf');
+        console.log('should show self', response.data);
+        yield put({ type: 'SET_SHELF', payload: response.data });
+    } catch (error) {
+        console.log('Shelf get request failed', error);
+    }
+}
+function* ShelfSaga() {
+    yield takeLatest('SAGA_FETCH_SHELF', fetchShelf);
+  }
+  
+export default ShelfSaga;
